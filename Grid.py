29b24374b1b37c7d607ff96b8429a61f9b8104af
rudolfla <rudolfbraungmail.com>
2021-05-29 10:43:46 +0200

    add player

--- Grid.py
+++ Grid.py
@@ -26,7 +26,7 @@ class Grid:
 
     def add_tile(self, x:int, y:int):
         newtile = TileBase()
-        self.tiles[self.width*x+y] = newtile
+        self.tiles[self.width*y+x] = newtile
 
     def show(self):
         text = ""
@@ -41,11 +41,60 @@ class Grid:
         print(text)   
 
 
+class PlayerGrid:
+    width = Grid.width
+    height = Grid.height
+    player= [None]*width*height
+
+    def add_player(self,x,y):
+        newplayer = Player()
+        self.player[self.width*y+x] = newplayer
+    
+    def move_player(self, x, y, dx, dy):
+        if dx != 0 or dy != 0:
+            self.player[self.width*(y+dy)+(x+dx)] = self.player[self.width*y+x]
+            self.player[self.width*y+x] = None
+    
+    def show(self):
+        text = ""
+        for x in range(self.width):
+            for y in range(self.height):
+                player = self.player[self.width*y+x]
+                if player:
+                    text += player.name
+                else:
+                    text += "_"
+            text += "\n"
+        print(text)   
+
+
+class Player:
+    name = "p"
+    hitpoints = 5
+
+
 grid = Grid()
-userinput = ""
-while userinput != "x":
+playergrid =PlayerGrid()
+userinputwhattodo = ""
+while userinputwhattodo != "x":
     grid.show()
+    playergrid.show()
     print("enter ...")
-    userinput = input()
-    if userinput == "w":
-        grid.add_tile(2,2)
+    userinputwhattodo = input()
+    if userinputwhattodo == "new tile":
+        print("insert x and y")
+        userinputx = int(input())
+        userinputy = int(input())
+        grid.add_tile(userinputx,userinputy)
+    if userinputwhattodo == "new player":
+        print("insert x and y")
+        userinputx = int(input())
+        userinputy = int(input())
+        playergrid.add_player(userinputx,userinputy)
+    if userinputwhattodo == "move player":
+        print("insert x and y and dx and dy")
+        userinputx = int(input())
+        userinputy = int(input())
+        userinputdx = int(input())
+        userinputdy = int(input())
+        playergrid.move_player(userinputx,userinputy,userinputdx,userinputdy)
