b04c8913a192a0868ce894039d1dd73a8aafcce2
rudolfla <rudolfbraungmail.com>
2021-05-29 13:45:42 +0200

    add unit/tile manipulation, sea tile with drowning

--- Grid.py
+++ Grid.py
@@ -1,7 +1,7 @@
 class TileBase:
     name = "-"
 
-    def on_enter(self):
+    def on_enter(self, unit):
         pass
 
     def on_damage(self, damage:int):
@@ -12,21 +12,60 @@ class TileForest(TileBase):
     name = "t"
     onfire = False
 
-    def on_enter(self):
+    def on_enter(self, unit):
         print("test")
 
     def on_damage(self, damage:int):
         self.onfire = True
 
+class TileSea(TileBase):
+    name = "~"
+    onfire = False
+
+    def on_enter(self, unit):
+        assert isinstance(unit, UnitBase)
+        if not unit.canswim:
+            unit.drown()
+
 
 class Grid:
-    width = 5
-    height = 6
-    tiles = [None]*width*height
+    def __init__(self):
+        self.height = 6
+        self.width = 5
+        self.tiles = [None]*self.width*self.height
+        self.units = [None]*self.width*self.height
 
-    def add_tile(self, x:int, y:int):
-        newtile = TileBase()
+    def add_tile(self, x:int, y:int, tiletype:TileBase=TileBase):
+        assert issubclass(tiletype, TileBase)
+        newtile = tiletype()
         self.tiles[self.width*y+x] = newtile
+
+    def add_unit(self,x,y):
+        newunit = UnitBase(grid=self, name="rudolf")
+        self.units[self.width*y+x] = newunit
+        newunit.pos = [x, y]
+            
+    def remove_unit(self, x:int, y:int):
+        if self.is_space_empty(False, x, y):
+            print(f"error try to remove unit at {x} {y} which does not exist.")
+            exit(1)
+        self.units[self.width*y+x] = None
+    
+    def move_unit(self, x:int, y:int, targetx:int, targety:int):
+        if self.is_space_empty(False, x, y):
+            print(f"error try to move unit at {x} {y} which does not exist.")
+            exit(1)    
+        if self.is_space_empty(False, targetx, targety) and not \
+                self.is_space_empty(True, targetx, targety):
+            unit = self.units.pop(self.width*y+x)
+            self.units[self.width*targety+targetx] = unit
+            unit.pos = [targetx, targety]
+            self.tiles[self.width*targety+targetx].on_enter(unit)
+            print(unit.pos)
+            
+    def is_space_empty(self, tiles:bool, x:int, y:int)->bool:
+        return x>=0 and x<self.width and y>=0 and y<self.height \
+            and not (self.tiles if tiles else self.units)[self.width*y+x]
 
     def show(self):
         text = ""
@@ -36,49 +75,41 @@ class Grid:
                 if tile:
                     text += tile.name
                 else:
-                    text += "~"
+                    text += " "
             text += "\n"
         print(text)   
 
 
-class PlayerGrid:
-    width = Grid.width
-    height = Grid.height
-    player= [None]*width*height
-
-    def add_player(self,x,y):
-        newplayer = Player()
-        self.player[self.width*y+x] = newplayer
+class UnitBase:
     
-    def move_player(self, x, y, dx, dy):
-        if dx != 0 or dy != 0:
-            self.player[self.width*(y+dy)+(x+dx)] = self.player[self.width*y+x]
-            self.player[self.width*y+x] = None
+    def __init__(self, grid:Grid, name:str="p", hitpoints:int=5, canswim:bool=False):
+        assert isinstance(grid, Grid)
+        self.name = name
+        self.hitpoints = hitpoints
+        self.canswim = canswim
+        self.grid = grid
+        self.pos = [None, None]
+
+    def drown(self):
+        print("I drowned :(")
+        self.grid.remove_unit(*self.pos)
     
-    def show(self):
-        text = ""
-        for x in range(self.width):
-            for y in range(self.height):
-                player = self.player[self.width*y+x]
-                if player:
-                    text += player.name
-                else:
-                    text += "_"
-            text += "\n"
-        print(text)   
-
-
-class Player:
-    name = "p"
-    hitpoints = 5
-
 
 grid = Grid()
-playergrid =PlayerGrid()
+grid2 = Grid()
 userinputwhattodo = ""
+
+grid.add_tile(2, 3, tiletype=TileForest)
+grid.add_tile(2, 4, tiletype=TileSea)
+grid.add_unit(2,3)
+grid.move_unit(2,3,2,4)
+
+print(grid.is_space_empty(True, 2, 3))
+print(grid2.is_space_empty(True, 2, 3))
+exit()
+
 while userinputwhattodo != "x":
     grid.show()
-    playergrid.show()
     print("enter ...")
     userinputwhattodo = input()
     if userinputwhattodo == "new tile":
@@ -86,15 +117,3 @@ while userinputwhattodo != "x":
         userinputx = int(input())
         userinputy = int(input())
         grid.add_tile(userinputx,userinputy)
-    if userinputwhattodo == "new player":
-        print("insert x and y")
-        userinputx = int(input())
-        userinputy = int(input())
-        playergrid.add_player(userinputx,userinputy)
-    if userinputwhattodo == "move player":
-        print("insert x and y and dx and dy")
-        userinputx = int(input())
-        userinputy = int(input())
-        userinputdx = int(input())
-        userinputdy = int(input())
-        playergrid.move_player(userinputx,userinputy,userinputdx,userinputdy)
