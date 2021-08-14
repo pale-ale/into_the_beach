692b18b4a9524dcb6c1475a0b87997d5f085b301
Tu2-prog <anhtuduong.nguyen@outlook.de>
2021-08-14 15:43:55 +0200

    mirror map design

--- itblib/Maps.py
+++ itblib/Maps.py
@@ -44,15 +44,56 @@ class MapGrasslands(Map):
     
     def __init__(self):
         super().__init__()
-        for x in range(0, 88):
-            self.tileids[x] = 2
-        for x in range(75, self.width*self.height):
+        for x in range(0, 100):
             self.tileids[x] = 1
-        for x in range(3):
-            self.tileids[x+85] = 5
-            self.tileids[x+95] = 5
-        for x in range(3, 6):
-            self.tileids[x] = 4
-        for x in range(74, 78):
+        for x in range (0, 5):
+            self.tileids[x] = 5
+        for x in range (10, 15):
+            self.tileids[x] = 5
+        for x in range (20, 25):
+            self.tileids[x] = 5
+        for x in range (30, 35):
+            self.tileids[x] = 5
+        for x in range (40, 45):
+            self.tileids[x] = 5
+        
+        for x in range(5, 10):
+            self.tileids[x] = 2
+        for x in range(15, 20):
+            self.tileids[x] = 2
+        for x in range(25, 30):
+            self.tileids[x] = 2
+        for x in range(35, 40):
+            self.tileids[x] = 2
+        for x in range(45, 50):
+            self.tileids[x] = 2
+        
+        for x in range (55, 60):
+            self.tileids[x] = 5
+        for x in range (65, 70):
+            self.tileids[x] = 5
+        for x in range (75, 80):
+            self.tileids[x] = 5
+        for x in range (85, 90):
+            self.tileids[x] = 5
+        for x in range (95, 100):
+            self.tileids[x] = 5
+
+        for x in range (50, 55):
+            self.tileids[x] = 2
+        for x in range (60, 65):
+            self.tileids[x] = 2
+        for x in range (70, 75):
+            self.tileids[x] = 2
+        for x in range (80, 85):
+            self.tileids[x] = 2
+        for x in range (90, 95):
+            self.tileids[x] = 2
+        for x in range(0, 10):
+            self.effectids[x].append(2+int(x/2))    
+        for x in range(75, 78):
             self.effectids[x].append(5)
-    
+        wheat = [57, 67, 22, 23, 24, 32, 42, 34, 44, 55, 56, 65, 43, 33, 66]
+        for x in wheat:
+            self.effectids[x].append(5)
+        
