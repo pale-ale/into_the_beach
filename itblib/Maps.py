90dc4c57dfbdc64993395d2a3b74fb71430777c0
Tu2-prog <anhtuduong.nguyen@outlook.de>
2021-08-15 14:24:29 +0200

    new map design

--- itblib/Maps.py
+++ itblib/Maps.py
@@ -47,18 +47,34 @@ class MapGrasslands(Map):
         for x in range(0, 100):
             self.tileids[x] = 1
         self.tileids[41] = 2
-        self.tileids[42] = 0
-        self.tileids[51] = 2
-        self.tileids[52] = 2
+        self.tileids[42] = 2
+        self.tileids[51] = 4
+        self.tileids[52] = 4
         self.tileids[44] = 3
-        self.tileids[45] = 0
+        self.tileids[45] = 3
         self.tileids[54] = 3
         self.tileids[55] = 3
         self.tileids[47] = 4
-        self.tileids[48] = 0
-        self.tileids[57] = 4
-        self.tileids[58] = 4
+        self.tileids[48] = 4
+        self.tileids[57] = 2
+        self.tileids[58] = 2
+        mountain = [9, 19, 8, 80, 90, 91]
+        for x in mountain:
+            self.effectids[x].append(3)
+        self.effectids[9].append(3)
+        town = [14, 15, 24, 25, 74, 75, 84, 85]
+        for x in town:
+            self.effectids[x].append(6)
+        wheat = [0, 1, 10]
+        for x in wheat:
+            self.effectids[x].append(5)
+        river = [61, 62, 37, 38]
+        for x in river:
+            self.effectids[x].append(4)  
+        self.effectids[11].append(7)
+        self.effectids[89].append(7)
+        self.effectids[33].append(2)
+        self.effectids[66].append(2)
+        self.effectids[36].append(2)
+        self.effectids[63].append(2)
 
-        for x in range(0, 10):
-            self.effectids[x].append(2+int(x/2))
-    
